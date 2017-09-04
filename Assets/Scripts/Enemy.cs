759b29bed5f116dd12f1826ac8314072c3f3ac0b
Finn Pinkenburg <finnpinkenburg@googlemail.com>
2017-09-04 15:37:36 +0200

    Enemy Pathfinding

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public int target = 0;
+    public Transform exitPoint;
+    public Transform[] wayPoints;
+    public float navigationUpdate;
+
+    private Transform enemy;
+    private float navigationTime = 0;
+
+    void Start()
+    {
+        enemy = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if (wayPoints != null)
+        {
+            navigationTime += Time.deltaTime;
+
+            if (navigationTime > navigationUpdate)
+            {
+                if (target < wayPoints.Length)
+                {
+                    enemy.position = Vector2.MoveTowards(enemy.position, wayPoints[target].position, navigationTime);
+                }
+                else
+                {
+                    enemy.position = Vector2.MoveTowards(enemy.position, exitPoint.position, navigationTime);
+                }
+
+                navigationTime = 0;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Checkpoint")
+        {
+            target += 1;
+        }
+        else if (other.tag == "Finish")
+        {
+            GameManager.instance.RemoveEnemyFromScreen();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    public GameObject spawnPoint;
+    public GameObject[] enemies;
+
+    public int maxEnemeiesOnScreen;
+    public int totalEnemies;
+    public int enemiesPerSpawn;
+
+    private int enemiesOnScreen = 0;
+    const float spawnDelay = 1f;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start ()
+    {
+        StartCoroutine(SpawnEnemies());
+	}
+
+    IEnumerator SpawnEnemies()
+    {
+        if (enemiesPerSpawn > 0 && enemiesOnScreen < totalEnemies)
+        {
+            for (int i = 0; i < enemiesPerSpawn; i++)
+            {
+                if (enemiesOnScreen < maxEnemeiesOnScreen)
+                {
+                    GameObject newEnemy = Instantiate(enemies[1]) as GameObject;
+                    newEnemy.transform.position = spawnPoint.transform.position;
+                    enemiesOnScreen += 1;
+                }
+            }
+        }
+        yield return new WaitForSeconds(spawnDelay);
+        StartCoroutine(SpawnEnemies());
+    }
+
+    public void RemoveEnemyFromScreen()
+    {
+        if (enemiesOnScreen > 0)
+        {
+            enemiesOnScreen -= 1;
+        }
+    }
+	
+}
