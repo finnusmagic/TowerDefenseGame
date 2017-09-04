1117f885c6a17b3419e1aee2b4ffe35c11d4ce03
Finn Pinkenburg <finnpinkenburg@googlemail.com>
2017-09-04 16:59:27 +0200

    Placement Fix

--- Assets/Scripts/Tower/TowerManager.cs
+++ Assets/Scripts/Tower/TowerManager.cs
@@ -1,15 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TowerManager : Singleton<TowerManager> {
 
-    TowerButton towerButtonPressed;
+    private TowerButton towerButtonPressed;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+
+            if (hit.collider.tag == "BuildSite")
+            {
+                hit.collider.tag = "BuildSiteFull";
+                PlaceTower(hit);
+            }
+        }
+    }
+
+    public void PlaceTower(RaycastHit2D hit)
+    {
+        if (!EventSystem.current.IsPointerOverGameObject() && towerButtonPressed != null)
+        {
+            GameObject newTower = Instantiate(towerButtonPressed.TowerObject);
+            newTower.transform.position = hit.transform.position;
+        }
+    }
 
     public void SelectedTower(TowerButton towerSelected)
     {
         towerButtonPressed = towerSelected;
-        Debug.Log("Pressed: " + towerButtonPressed.gameObject);
     }
 
 }
